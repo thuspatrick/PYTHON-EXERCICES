ef93178f82d92f0f09aa03e399ad407af55b6116
Patrick Montheu <thuspatrick@gmail.com>
2020-01-24 13:34:40 +0100

    playing with tuples

--- exercices/HelloWorld.py
+++ exercices/HelloWorld.py
@@ -1,5 +1,15 @@
+# Nettoyer la console avant le demarrage du programme #
 import os
 os.system('clear')
+#######################################################
 user = "patrick"
-print("Bonjour tout le monde...")
-print("Bonjour " + user)
+print("1 - Bonjour tout le monde...")
+print("2 - Bonjour " + user)
+##################
+# Les tuples #
+#################
+tuple1 = ("Angelina", "Gabriela", "Daniela")
+tuple2 = ("Flore",)
+mytuple = tuple1 + tuple2
+
+print(mytuple)
